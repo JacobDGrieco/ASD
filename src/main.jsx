7e8f21679d676fd988f35c5e89c6999f7bf69cad
JacobDGrieco <jacobdgrieco@gmail.com>
2026-04-27 10:04:32 -0400

    [deploy] removed Analytics logs

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<BrowserRouter>
 			<App />
-			<Analytics />
+			<Analytics debug={false} />
 		</BrowserRouter>
 	</StrictMode>
 );
